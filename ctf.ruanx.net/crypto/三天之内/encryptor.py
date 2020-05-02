c049e5643852f70e0522e29b1a3ca412a7b31316
memset0 <memset0@outlook.com>
2020-05-02 21:44:38 +0800

    三天之内 #0

--- ctf.ruanx.net/crypto/三天之内/encryptor.py
+++ ctf.ruanx.net/crypto/三天之内/encryptor.py
@@ -1,16 +1,12 @@
 from Crypto.Cipher import AES
-import os
+from secret import flag
 import time
-# from secret import flag
 from hashlib import md5
 import base64
 
-flag = '123456'
-# key = md5(str(int(time.time())).encode()).digest()
-key = os.urandom(16)
+key = md5(str(int(time.time())).encode()).digest()
 aes = AES.new(key, AES.MODE_ECB)
-outData = aes.encrypt(flag)
-# print(str(outData))
+outData = aes.encrypt(flag.encode('utf-8'))
 
 with open('flag.enc', 'wb') as f:
     f.write(base64.b64encode(outData))
