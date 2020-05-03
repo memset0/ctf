6e3a26ec1c75c6584bd84f0c620ab43541da9dc2
memset0 <memset0@outlook.com>
2020-05-03 20:44:29 +0800

    [TOOL]RsaCtfTool remove

--- tools/RsaCtfTool/attacks/single_key/nullattack.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-def attack(attack_rsa_obj, publickey, cipher=[]):
-    """Do nothing, used for multi-key attacks that succeeded so we just print the
-       private key without spending any time factoring
-    """
-    return (None, None)
